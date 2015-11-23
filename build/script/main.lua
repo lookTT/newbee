ed310f84e50ac9a16bdf3fd09d8e89cc4ccd98a1
lookTT <zltdhr@gmail.com>
2015-11-23 16:10:55 +0800

    Update main.lua
    将lua调试语句注释

--- build/script/main.lua
+++ build/script/main.lua
@@ -1,5 +1,5 @@
---如果不需要debugger注释掉即可
-require("debugger")("192.168.1.95", "10000", "luaidekey")
+--如果需要debugger去掉注释掉即可 HOST填写调试IP 作者使用LDT作为编辑调试工具
+--require("debugger")("127.0.0.1", "10000", "luaidekey")
 --Initialization necessary information
 local nb = require("nb")
 require("init")
